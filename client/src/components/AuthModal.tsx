4b84d62ab544f86d9c8e58c115ec4cdfcd70975b
reubenc-bit <69934343+reubenc-bit@users.noreply.github.com>
2025-08-18 21:50:20 +0200

    Update AuthModal.tsx

--- client/src/components/AuthModal.tsx
+++ client/src/components/AuthModal.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "@/hooks/useAuth";
-import { PackageSelector } from "./PackageSelector";
+import { PackageSelector } from "./PackageSelector.tsx";
 
 
 interface AuthModalProps {
